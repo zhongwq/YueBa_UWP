83ec6a471d3b510413ca2b739e6fb30f4bbf7e1d
Novemb11 <645592938@qq.com>
2018-05-30 17:08:53 +0800

    11: Finish Index Page

--- YueBa/Views/Index.xaml.cs
+++ YueBa/Views/Index.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +13,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using YueBa.Global;
+using System.Collections.ObjectModel;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -22,9 +27,42 @@ namespace YueBa.Views
     /// </summary>
     public sealed partial class Index : Page
     {
+        private Events MyEvents { get; set; }
+        private Store store;
         public Index()
         {
             this.InitializeComponent();
         }
+
+        private ObservableCollection<EventItem> test = new ObservableCollection<EventItem>();
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            store = Store.getInstance();
+            if (store != null)
+            {
+                MyEvents = await Services.EventServices.getAllEventsParticipatesIn(store.token);
+                foreach (var element in MyEvents.events)
+                {
+                    element.img = Global.Config.api + element.img;
+                    test.Add(element);
+                }
+
+
+            }
+
+
+        }
+
+
+        private void EventItem_ItemClicked(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+
     }
+
+
 }
